9ab09f282fde8dd7b5bedb95fb1b8531c4fab8f0
Travis Biehn <tbiehn@gmail.com>
2011-09-23 14:13:17 -0400

    Added LegacyAdapterImageLoader for API Levels under 9 (Gingerbread)

--- src/com/travisbiehn/android/util/LegacyAdapterImageLoader.java
+++ src/com/travisbiehn/android/util/LegacyAdapterImageLoader.java
@@ -5,30 +5,32 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadFactory;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.AdapterView;
 
 /**
+ * Legacy version for pre API 9 devices.
  * Provides multithreaded asynchronous image loading for AdapterViews,
  * optimized. Pass in only one LoadPair or List<LoadPair> per row!
  * 
- * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details.
+ * This program is free software. It comes without any warranty, to the extent
+ * permitted by applicable law. You can redistribute it and/or modify it under
+ * the terms of the Do What The Fuck You Want To Public License, Version 2, as
+ * published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more
+ * details.
  * 
  * @author Travis Biehn
  * 
  */
-public class AdapterImageLoader {
+public class LegacyAdapterImageLoader {
 	/**
 	 * Provides callback functionality for image grabs.
 	 * 
@@ -69,12 +71,14 @@ public class AdapterImageLoader {
 	 * 
 	 * @param <T>
 	 */
-	public class FixedStack<T> extends LinkedBlockingDeque<T> {
+	public class FixedStack<T> extends Stack<T> {
 		private static final long serialVersionUID = 1L;
 		private static final int KLUDGEFACTOR = 3; // Magical kludge constant
 
 		private int maxEntries() {
 			// Visibile portion of the adapterView, might break, might not.
+			Log.v("AdapterImageLoader", "Visible Portion: "+(adapterView.getLastVisiblePosition() - adapterView
+					.getFirstVisiblePosition())); // Starts from negative numbers....
 			return (adapterView.getLastVisiblePosition() - adapterView
 					.getFirstVisiblePosition()) + KLUDGEFACTOR;
 		}
@@ -83,19 +87,29 @@ public class AdapterImageLoader {
 		}
 
 		public boolean add(T x) {
-			super.addFirst(x);
-			this.trimEntries();
+			synchronized (this) {
+				super.push(x);
+				this.trimEntries();
+				this.notify();
+			}
 			return true;
+
 		}
 
 		public void addFirst(T x) {
-			super.addFirst(x);
-			this.trimEntries();
+			synchronized (this) {
+				super.push(x);
+				this.trimEntries();
+				this.notify();
+			}
 		}
 
 		public void addLast(T x) {
-			throw new UnsupportedOperationException(
-					"addLast not supported by this stack.");
+			synchronized (this) {
+
+				throw new UnsupportedOperationException(
+						"addLast not supported by this stack.");
+			}
 		}
 
 		/**
@@ -105,8 +119,30 @@ public class AdapterImageLoader {
 			int toTrim = super.size() - maxEntries();
 			if (toTrim > 0) {
 				for (int i = 0; i < toTrim; i++) {
-					super.pollLast(); // Pop off the end (oldest) entries.
+					super.remove(super.size() - 1); // Pop off the end (oldest)
+													// entries.
 				}
+			}
+		}
+
+		/**
+		 * Blocks until an item is available to grab.
+		 * 
+		 * @return
+		 * @throws InterruptedException
+		 */
+		public T takeFirst() throws InterruptedException {
+			synchronized (this) {
+				T ret = null;
+				while (ret == null) {
+					try {
+						ret = super.pop();
+					} catch (Exception e) {
+						// Wait, then rexecute.
+						this.wait();
+					}
+				}
+				return ret;
 			}
 		}
 
@@ -159,12 +195,12 @@ public class AdapterImageLoader {
 							final BitmapCallback callback = pair.callback;
 							final URLConnection connection = newurl
 									.openConnection();
-							//If you have a cache implementation, use it.
+							// If you have a cache implementation, use it.
 							connection.setUseCaches(true);
 
 							final Bitmap out = BitmapFactory
 									.decodeStream(connection.getInputStream());
-							//Post to callback in the UI thread.
+							// Post to callback in the UI thread.
 							handler.post(new Runnable() {
 								@Override
 								public void run() {
@@ -212,9 +248,11 @@ public class AdapterImageLoader {
 		le.list.add(new LoadPair(url, callback));
 		workQueue.add(le);
 	}
-	
+
 	/**
-	 * You must make only one call to this class per row entry! For multiple images per row use addLoadPairs()
+	 * You must make only one call to this class per row entry! For multiple
+	 * images per row use addLoadPairs()
+	 * 
 	 * @param loadPair
 	 */
 	public void addLoadPair(LoadPair loadPair) {
@@ -223,9 +261,10 @@ public class AdapterImageLoader {
 		le.list.add(loadPair);
 		workQueue.add(le);
 	}
-	
+
 	/**
 	 * You must make only one call to this class per row entry.
+	 * 
 	 * @param loadPairs
 	 */
 	public void addLoadPairs(List<LoadPair> loadPairs) {
